894c60679ed41eeb23a21b97c1d1e54e2dab4a03
okhorosko <olexgood@gmail.com>
2024-09-18 14:01:28 +0300

    update

--- pages/product.page.ts
+++ pages/product.page.ts
@@ -4,7 +4,7 @@ export class Product extends AppComponent{
     [x: string]: any;
 
     async addToCart(){
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 seconds
+        await new Promise(resolve => setTimeout(resolve, 2000)); // Wait for 2 seconds
         await this.page.getByRole('button', { name: 'Add to Cart' }).click();
         await this.page.getByRole('button', { name: '   Cart' }).click();
 
